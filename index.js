2be8e8cf7c75444010d5e9aaaf47ef35a91648b2
Piseth-Pon <ponpiseth8@gmail.com>
2024-05-26 13:46:33 +0700

    initial setup

---
+++ index.js
@@ -0,0 +1,11 @@
+import PocketBase from './pocketbase.es.mjs'
+
+const url = ''
+const client = new PocketBase(url)
+
+
+window.addEventListener("DOMContentLoaded", async () => {
+
+  console.log("everything is ready")
+
+})
